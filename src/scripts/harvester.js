f9d74f58fb93c79b6cccb7e218ddd461cc8a7807
Swathi Katakam <swathik2322@gmail.com>
2024-01-29 10:55:23 -0600

    modern-farm into my repository

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,10 +1,3 @@
-import { createAsparagus } from "./seeds/asparagus.js"
-import { createCorn } from "./seeds/corn.js";
-import { createPotato } from "./seeds/potato.js";
-import { createSoybean } from "./seeds/soybean.js";
-import { createSunflower } from "./seeds/sunflower.js";
-import { createWheat } from "./seeds/wheat.js";
-
 const harvestedFood=[];
 
 const harvestPlantLoop=(plantObject)=>{    
